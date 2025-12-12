757f1fa47988aa7c3644713b35ea67a5478193ce
Julio Pedro López Bayón <juliolb@educastur.org>
2025-12-12 17:51:35 +0100

    Actualización

--- src/main/java/unidad3/aritmético/ServiceTask.java
+++ src/main/java/unidad3/aritmético/ServiceTask.java
@@ -1,29 +1,55 @@
 package unidad3.aritmético;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.EOFException;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ServiceTask {
 
 	private final Socket socket;
+	private static final String numberRegex = "";
+	private static final String operatorRegex = "[\\+\\-×÷]";
+	private static final Pattern p = Pattern.compile(String.format("(%s)(%s)(%s)", numberRegex, operatorRegex, numberRegex));
 
-	public ServiceTask(Socket socket) {
+	public ServiceTask(Socket socket) throws SocketException {
 		this.socket = socket;
+		socket.setSoTimeout(10000);
 	}
 	
 	public void run() {
 		try (socket) {
-			DataInputStream in = new DataInputStream(socket.getInputStream());
-			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-			try {
-				Thread.sleep(10000);
-			} catch (InterruptedException e) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+			Matcher m = p.matcher(in.readLine());
+			if (m.matches()) {
+				double leftOp = Double.parseDouble(m.group(1));
+				double rightOp = Double.parseDouble(m.group(3));
+				switch (m.group(2)) {
+				case "+":
+					out.println(String.valueOf(leftOp + rightOp));
+					break;
+				case "-":
+					out.println(String.valueOf(leftOp - rightOp));
+					break;
+				case "×":
+					out.println(String.valueOf(leftOp * rightOp));
+					break;
+				case "÷":
+					out.println(String.valueOf(leftOp / rightOp));
+					break;
+				}
+				
 			}
+			else
+				out.println("expresión incorrecta");
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println(socket.getRemoteSocketAddress() + ":" + socket.getPort() + ":" + e.getLocalizedMessage());
 		}
 		System.out.println(socket.getRemoteSocketAddress() + ": conexión finalizada");
 	}
