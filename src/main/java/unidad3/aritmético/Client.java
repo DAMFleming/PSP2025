7c3f2a502de4b30e2d854af8830b0cfdb57b159e
julio <julio@void.jplb.org>
2025-12-14 21:28:11 +0100

    Actualización

--- src/main/java/unidad3/aritmético/Client.java
+++ src/main/java/unidad3/aritmético/Client.java
@@ -1,27 +1,6 @@
 package unidad3.aritmético;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Font;
-import java.awt.FontFormatException;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.Socket;
-
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
-
-// 012345 ÷×−,. áéíóúäñ
 
 public class Client {
 
@@ -35,11 +14,13 @@ public class Client {
 	private boolean sent;
 	private static Font displayFont;
 	private static Font buttonFont;
+	private GridBagLayout keyboardLayout = new GridBagLayout();
+	private GridBagConstraints constraints = new GridBagConstraints();
 	
 	private Client() {
 		try {
-			displayFont = Font.createFont(Font.PLAIN, Client.class.getResourceAsStream("/fonts/LEDCalculator.ttf")).deriveFont(40f);
-			buttonFont = Font.createFont(Font.PLAIN, Client.class.getResourceAsStream("/fonts/FredokaOne.ttf")).deriveFont(30f);
+			displayFont = Font.createFont(Font.PLAIN, Client.class.getResourceAsStream("/fonts/LEDCalculator.ttf")).deriveFont(30f);
+			buttonFont = Font.createFont(Font.PLAIN, Client.class.getResourceAsStream("/fonts/FredokaOne.ttf")).deriveFont(35f);
 			frame = new JFrame("Calculadora Cliente");
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			Container c = frame.getContentPane();
@@ -54,11 +35,8 @@ public class Client {
 			display.setBorder(BorderFactory.createCompoundBorder(
 					display.getBorder(),
 					BorderFactory.createEmptyBorder(10, 10, 10, 10)));
-			c.add(panel, BorderLayout.NORTH);
-			JPanel p = new JPanel(new BorderLayout());
-			p.add(new MainKeyboard(), BorderLayout.CENTER);
-			c.add(p, BorderLayout.CENTER);
-			c.add(new OperatorsKeyboard(), BorderLayout.EAST);
+			c.add(panel, BorderLayout.NORTH);	
+			c.add(new MainKeyboard(), BorderLayout.CENTER);
 			frame.pack();
 			frame.setLocationRelativeTo(null);
 		} catch (FontFormatException | IOException e) {
@@ -99,56 +77,51 @@ public class Client {
     private class MainKeyboard extends JPanel {
     	private static final long serialVersionUID = 1L;
     	public MainKeyboard() {
-    		setLayout(new GridLayout(4, 3));
-    		setBorder(BorderFactory.createCompoundBorder(
-    				BorderFactory.createEmptyBorder(10, 10, 10, 10),
-    				getBorder()));
-    		add(new NumberKey("7"));
-    		add(new NumberKey("8"));
-    		add(new NumberKey("9"));
-    		add(new NumberKey("4"));
-    		add(new NumberKey("5"));
-    		add(new NumberKey("6"));
-    		add(new NumberKey("1"));
-    		add(new NumberKey("2"));
-    		add(new NumberKey("3"));
-    		add(new NumberKey("0"));
+    		super(keyboardLayout);
+    		setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+    		add(new NumberKey("7", 0, 0));
+    		add(new NumberKey("8", 0, 1));
+    		add(new NumberKey("9", 0, 2));
+    		add(new NumberKey("4", 1, 0));
+    		add(new NumberKey("5", 1, 1));
+    		add(new NumberKey("6", 1, 2));
+    		add(new NumberKey("1", 2, 0));
+    		add(new NumberKey("2", 2, 1));
+    		add(new NumberKey("3", 2, 2));
+    		add(new NumberKey("0", 3, 0));
     		add(new DecimalKey());
     		add(new ClearKey());
-    	}
-    }
-    
-    private class OperatorsKeyboard extends JPanel {
-    	private static final long serialVersionUID = 1L;
-    	public OperatorsKeyboard() {
-    		setLayout(new GridLayout(5, 1));
-    		setBorder(BorderFactory.createCompoundBorder(
-    				BorderFactory.createEmptyBorder(10, 0, 10, 10),
-    				getBorder()));
-    		add(new OperatorKey("÷"));
-    		add(new OperatorKey("×"));
+    		add(new OperatorKey("÷", 0, 3));
+    		add(new OperatorKey("×", 1, 3));
     		add(new MinusKey());
-    		add(new OperatorKey("+"));
+    		add(new OperatorKey("+", 3, 3));
     		add(new EqualsKey());
     	}
     }
-    
+       
     private abstract class Key extends JButton {
     	private static final long serialVersionUID = 1L;
-    	public Key(String text) {
+    	public Key(String text, int row, int column, int width, int height) {
     		super(text);
     		setFocusable(false);
     		setFont(buttonFont);
     		setForeground(Color.DARK_GRAY);
     		addActionListener(this::listener);
+    		constraints.gridx = column;
+    		constraints.gridy = row;
+    		constraints.gridwidth = width;
+    		constraints.gridheight = height;
+    		constraints.fill = GridBagConstraints.HORIZONTAL;
+    		constraints.insets = new Insets(3, 3, 3, 3);
+    		keyboardLayout.setConstraints(this, constraints);
     	}
     	protected abstract void listener(ActionEvent e);
     }
     
     private class NumberKey extends Key {
     	private static final long serialVersionUID = 1L;
-    	public NumberKey(String text) {
-    		super(text);
+    	public NumberKey(String text, int row, int column) {
+    		super(text, row, column, 1, 1);
     	}
 		protected void listener(ActionEvent e) {
     		if (sent)
@@ -164,7 +137,7 @@ public class Client {
     private class EqualsKey extends Key {
     	private static final long serialVersionUID = 1L;
 		public EqualsKey() {
-    		super("=");
+    		super("=", 4, 2, 2, 1);
     	}
     	protected void listener(ActionEvent e) {
     		if (rightOp)
@@ -175,7 +148,7 @@ public class Client {
     private class DecimalKey extends Key {
     	private static final long serialVersionUID = 1L;
 		public DecimalKey() {
-    		super(".");
+    		super(".", 3, 1, 2, 1);
     	}
     	protected void listener(ActionEvent e) {
     		if (sent)
@@ -190,7 +163,7 @@ public class Client {
     private class ClearKey extends Key {
     	private static final long serialVersionUID = 1L;
 		public ClearKey() {
-    		super("C");
+    		super("C", 4, 0, 2, 1);
     	}
     	protected void listener(ActionEvent e) {
    			reset();
@@ -200,7 +173,7 @@ public class Client {
     private class MinusKey extends Key {
     	private static final long serialVersionUID = 1L;
     	public MinusKey() {
-    		super("-");
+    		super("-", 2, 3, 1, 1);
     	}
     	protected void listener(ActionEvent e) {
     		if (sent)
@@ -216,8 +189,8 @@ public class Client {
     
     private class OperatorKey extends Key {
     	private static final long serialVersionUID = 1L;
-		public OperatorKey(String text) {
-    		super(text);
+		public OperatorKey(String text, int row, int column) {
+    		super(text, row, column, 1, 1);
     	}
     	protected void listener(ActionEvent e) {
     		if (leftOp && !operator)
